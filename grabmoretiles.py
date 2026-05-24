f1a1e03e5b4c3cba7bc7693fb6b9016bce55e6e6
supertoaster0 <lifengx666@gmail.com>
2026-05-24 15:39:01 -0400

    Able to grab leftmost 2 tiles :D

---
+++ grabmoretiles.py
@@ -0,0 +1,138 @@
+from pybricks.parameters import Port, Stop
+from pybricks.tools import wait, StopWatch
+from line_follower import pid_line_follower
+from config import ev3, left_motor, right_motor, motor_a, motor_d, colorsensorLeft, colorsensorRight
+from outil import move_motors
+from finding_mozaic_pieces import grab_tiles
+
+"""
+REMINDER:
+CLOCKWISE = --
+COUNTER-CLOCKWISE = ++
+FORWARD = -+
+BACKWARD = +-
+
+COLORS:
+YELLOW = 1
+BLUE = 2
+GREEN = 3
+WHITE = 4
+
+MOZAIC:
+    0, 1, 2, 3
+    4, 5, 6, 7
+    8, 9, 10, 11
+"""
+
+def grab_second_four_tiles(mosaic_pattern: list, color_arrays: list):
+    grabbed_tiles = [
+        0, 0,
+        0, 0,
+    ]
+    move_motors(300, 300, rotations=1.48)
+    #Follow The Line until it reaches the center
+    #turn 90 degrees clockwise
+    grabbed_tiles, color_arrays[0], color_arrays[1], color_arrays[2], color_arrays[3] = grabLeft2Tiles(mosaic_pattern, grabbed_tiles, color_arrays)
+    grabbed_tiles, color_arrays[0], color_arrays[1], color_arrays[2], color_arrays[3] = grabRight2Tiles(mosaic_pattern, grabbed_tiles, color_arrays)
+    
+
+def grabLeft2Tiles(mosaic_pattern: list, grabbed_tiles: list, color_arrays: list):
+    if mosaic_pattern[8] == mosaic_pattern[9]:
+        MoveToColor(mosaic_pattern[8], 2.5)
+        grabbed_tiles, color_arrays = grab_similar(grabbed_tiles, mosaic_pattern[8], color_arrays)
+        grabbed_tiles[0] = mosaic_pattern[8]
+        grabbed_tiles[1] = mosaic_pattern[8]
+    else:
+        MoveToColor(min(mosaic_pattern[8], mosaic_pattern[9]), 2.5)
+        grabbed_tiles, color_arrays = grab_else(grabbed_tiles, mosaic_pattern[8], mosaic_pattern[9], color_arrays)
+    return grabbed_tiles, color_arrays[0], color_arrays[1], color_arrays[2], color_arrays[3]
+
+def grabRight2Tiles(mosaic_pattern: list, grabbed_tiles: list, color_arrays: list):
+
+    return grabbed_tiles, color_arrays[0], color_arrays[1], color_arrays[2], color_arrays[3]
+
+def grab_similar(grabbed_tiles: list, color_index: int, color_arrays):
+    left = GetClosestAvailableTiles(color_arrays[color_index - 1], -1)
+    right = GetClosestAvailableTiles(color_arrays[color_index - 1], 1)
+    if left == right:
+        color_arrays[color_index - 1] = grab_tiles(color_arrays[color_index - 1], left, 0, grabbed_tiles)
+    else:
+        if left > right:
+            color_arrays[color_index - 1] = grab_tiles(color_arrays[color_index - 1], right, 1, grabbed_tiles)
+            color_arrays[color_index - 1] = grab_tiles(color_arrays[color_index - 1], left, -1, grabbed_tiles)
+        else:
+            color_arrays[color_index - 1] = grab_tiles(color_arrays[color_index - 1], left, -1, grabbed_tiles)
+            color_arrays[color_index - 1] = grab_tiles(color_arrays[color_index - 1], right, 1, grabbed_tiles)
+    return grabbed_tiles, color_arrays
+
+def grab_else(GrabbedTiles, LeftColor, RightColor, ColorArrays, first : bool = true):
+    left = GetClosestAvailableTiles(ColorArrays[LeftColor - 1], -1)
+    right = GetClosestAvailableTiles(ColorArrays[RightColor - 1], 1)
+    firstcolor = min(LeftColor, RightColor)
+    secondcolor = max(LeftColor, RightColor)
+    if firstcolor == left:
+        firstdir = -1
+    else:
+        firstdir = 1
+    seconddir = firstdir*-1
+    if firstdir == -1:
+        ColorArrays[firstcolor - 1] = grab_tiles(ColorArrays[firstcolor - 1], left, firstdir, GrabbedTiles)
+    else:
+        ColorArrays[firstcolor - 1] = grab_tiles(ColorArrays[firstcolor - 1], right, firstdir, GrabbedTiles)
+        
+    MoveToColor(secondcolor, firstcolor)
+
+    if seconddir == 1:
+        ColorArrays[secondcolor - 1] = grab_tiles(ColorArrays[secondcolor - 1], right, seconddir, GrabbedTiles)
+    else:
+        ColorArrays[secondcolor - 1] = grab_tiles(ColorArrays[secondcolor - 1], left, seconddir, GrabbedTiles)
+    
+    if first:
+        if firstdir == 1:
+            GrabbedTiles[0] = secondcolor
+            GrabbedTiles[1] = firstcolor
+        else:
+            GrabbedTiles[1] = secondcolor
+            GrabbedTiles[0] = firstcolor
+    return GrabbedTiles, ColorArrays
+
+def MoveToColor(target_color: int, starting_color: int) -> None:
+    get_distance = 0.5 * (target_color - starting_color)
+    if get_distance > 0:
+        pid_line_follower(follow_sensor_port=Port.S4,
+                stop_sensor_port=Port.S1,
+                base_speed=300,
+                Kp=3, Kd=3, Ki=0,
+                target=48,
+                max_angle=None,
+                stop_mode="c",
+                stop_threshold=22,
+                side="l",
+                stop_count=int(get_distance / 0.5))
+        wait(250)
+        move_motors(-300, 300, rotations=0.24)
+        left_motor.hold()
+        right_motor.hold()
+    else:
+        move_motors(300, 300, rotations=1.54)
+        pid_line_follower(follow_sensor_port=Port.S1,
+                stop_sensor_port=Port.S4,
+                base_speed=300,
+                Kp=3, Kd=3, Ki=0,
+                target=48,
+                max_angle=None,
+                stop_mode="c",
+                stop_threshold=22,
+                side="r",
+                stop_count=int(abs(get_distance) / 0.5))
+        wait(100)
+        move_motors(-300, 300, rotations=0.28)
+        wait(100)
+        move_motors(300, 300, rotations=1.54)
+
+def GetClosestAvailableTiles(color_list, dir):
+    index = 0
+    for i in range(3):
+        if color_list[i][dir]:
+            index = min(index, i)
+    return index
